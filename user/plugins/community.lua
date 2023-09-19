03e0b4643fe2d94922eabeaca750d8ec17348ef9
Back Nix <back2nix@list.ru>
2023-09-19 23:18:53 +0300

    make push

--- user/plugins/community.lua
+++ user/plugins/community.lua
@@ -1,6 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
-  -- { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.json" },
@@ -14,6 +14,12 @@ return {
   { import = "astrocommunity.editing-support.multicursors-nvim"},
   { import = "astrocommunity.lsp.inc-rename-nvim"},
   { import = "astrocommunity.editing-support.refactoring-nvim" },
-  { import = "astrocommunity.pack.full-dadbod" },
+  { import = "astrocommunity.pack.full-dadbod",
+    init = function()
+      -- Your DBUI configuration
+      -- vim.g.db_ui_disable_mappings = 1
+      -- vim.g.dbs = { dev = 'mysql://root@localhost:3306/mydatabase' }
+    end,
+  },
   { import = "astrocommunity.pack.docker" },
 }
