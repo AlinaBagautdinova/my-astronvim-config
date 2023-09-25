b1f5f3ca826a25b37d77d632f11486ca340910ac
back2nix <back2nix@list.ru>
2023-09-25 17:55:38 +0300

    make push

--- user/plugins/community.lua
+++ user/plugins/community.lua
@@ -23,4 +23,26 @@ return {
   },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  -- { "mg979/vim-visual-multi",
+  --   branch = "master",
+  --   keys = {
+  --     "<S-Left>",
+  --     "<C-Down>",
+  --     "<C-Up>",
+  --     "<S-Right>",
+  --     "<C-LeftMouse>",
+  --     "<C-RightMouse>",
+  --     "<M-C-RightMouse>",
+  --     { "<C-n>", mode = { "n", "x" } },
+  --     { "<Bslash><Bslash><Bslash>", mode = { "n", "x" } },
+  --   },
+  --   cmd = { "VMClear", "VMDebug", "VMFromSearch", "VMLive", "VMRegisters", "VMSearch", "VMTheme" },
+  --   init = function()
+  --     -- require("wuelnerdotexe.plugin.util").add_colorscheme_integration("visual-multi")
+  --
+  --     vim.api.nvim_set_var("VM_show_warnings", 0)
+  --     vim.api.nvim_set_var("VM_mouse_mappings", 1)
+  --     vim.api.nvim_set_var("VM_maps", { Undo = "u", Redo = "<C-r>" })
+  --   end,
+  -- },
 }
