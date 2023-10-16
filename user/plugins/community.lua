e2b526436db5173447c7a8ec52819138bc491d65
back2nix <back2nix@list.ru>
2023-10-16 12:57:06 +0300

    make push

--- user/plugins/community.lua
+++ user/plugins/community.lua
@@ -11,9 +11,20 @@ return {
   { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.nix" },
-  { import = "astrocommunity.editing-support.multicursors-nvim" },
+  {
+    import = "astrocommunity.editing-support.multicursors-nvim",
+    -- keys = {
+    --   {
+    --     mode = { "v", "n" },
+    --     "<Leader>m",
+    --     "<cmd>MCstart<cr>",
+    --     desc = "Create a selection for selected text or word under the cursor",
+    --   },
+    -- },
+  },
   { import = "astrocommunity.lsp.inc-rename-nvim" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity.editing-support.nvim-treesitter-endwise" },
   {
     -- https://www.youtube.com/watch?v=NhTPVXP8n7w&list=PLmcTCfaoOo_grgVqU7UbOx7_RG9kXPgEr&index=16&ab_channel=DevOpsToolbox
     import = "astrocommunity.pack.full-dadbod",
